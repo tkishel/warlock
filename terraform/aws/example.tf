7e510fb613a5e9c918dbaed898d630e7adccea8b
Tom Kishel <tkishel@TomKishelWiz.local>
2023-11-21 14:56:42 -0800

    use pgess version

--- terraform/aws/example.tf
+++ terraform/aws/example.tf
@@ -1,8 +1,7 @@
 # Wiz Example
 
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "example-bucket"
-  acl    = "private"
+resource "aws_s3_bucket" "wiz" {
+  bucket = "my-tf-test-bucket"
   versioning {
     enabled = true
   }
